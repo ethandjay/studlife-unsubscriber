29b708ce794fd096c49bf7b465de3e330b2256d2
Ethan Jaynes <ethandjay@gmail.com>
2018-09-19 22:52:05 -0500

    MORE buggy

--- main.py
+++ main.py
@@ -32,9 +32,11 @@ def submit():
         email = request.form['email']
         
         SCOPES = ['https://www.googleapis.com/auth/admin.directory.group.member', 'https://www.googleapis.com/auth/admin.directory.group']
+        # SERVICE_ACCOUNT_JSON = 'studlife-unsubscriber-cc008c44e4ba.json'
         SERVICE_ACCOUNT_JSON = json.loads(os.environ['studlife-unsubscriber-cc008c44e4ba.json'])
 
         credentials = service_account.Credentials.from_service_account_info(SERVICE_ACCOUNT_JSON, scopes=SCOPES)
+        # credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_JSON, scopes=SCOPES)
         delegated_credentials = credentials.with_subject('ethan.jaynes@studlife.com')
     
         admin = googleapiclient.discovery.build('admin', 'directory_v1', credentials=delegated_credentials)
@@ -43,7 +45,7 @@ def submit():
             noneWorked = True
             for tup in sections[1:]:
                 try:
-                    admin.members().delete(groupKey=f'{top[1]}@studlife.com', memberKey=f'{email}').execute()
+                    admin.members().delete(groupKey=f'{tup[1]}@studlife.com', memberKey=f'{email}').execute()
                 except:
                     pass
                 else:
@@ -60,7 +62,7 @@ def submit():
                 alert = {'status':'danger', 'message': f'Couldn\'t unsubscribe. Make sure you selected the right section and entered in your email correctly.'}
             else:
                 alert = {'status':'success', 'message': f'{email} successfully unsubscribed from {section}@studlife.com'}
-                
+
 
     return render_template('index.html', sections=sections, alert=alert)
 
